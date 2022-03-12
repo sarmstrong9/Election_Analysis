862f4b38c906b2f261c251a0528b51cc87cbe4e8
Armstrong <Scott.Armstrong@viasat.com>
2022-03-11 16:42:36 -0800

    % votes complete

--- PyPoll.py
+++ PyPoll.py
@@ -2,23 +2,63 @@
 import os
 #import csv module
 import csv
-
 # Assign variable and Read csv file
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+
+# 1. Initialize a total vote counter
+total_votes = 0
+
+# Candidate options List
+candidate_options = []
+# Create empty dictionary
+candidate_votes = {}
+
 # Open the election results and read file
 with open(file_to_load) as election_data:
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    #Read and print the header row
+    #Read the header row
     headers = next(file_reader)
-    print(headers) 
+    
+    # Print each row in the CSV file
+    for row in file_reader:
+        # Add to the total vote count
+        total_votes += 1
+        
+        # Print the candidate name for each row
+        candidate_name = row[2]
 
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
 
+            # Begin tracking that candidates vote count
+            candidate_votes[candidate_name] =0
 
-# The Data we need to retreive
+        # Add a vote to that candidates count
+        candidate_votes[candidate_name] += 1
 
-#1. The total number of votes cast
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list.     
+    for candidate_name in candidate_votes:
+        # Retreieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        #Calculate the % of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #Print the candidate name and percentage of votes
+        print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+        
+# 3. Print the total votes
+print(total_votes) 
+# Print the candidate list
+print(candidate_options) 
+# Print Candiate vote dictionary
+print(candidate_votes)
 
 #2. A complete list of candidates who received votes
 
@@ -31,8 +71,7 @@ with open(file_to_load) as election_data:
 
 ## Write data to new text file
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # using the with statement open the file as a text file
 with open(file_to_save, "w") as txt_file:
     # write three counties to the File
